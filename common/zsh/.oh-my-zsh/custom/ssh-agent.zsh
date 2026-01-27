5200d6134f14eccc425c9464f0a1ad49ae297f15
FridrichMethod <zhaoyangli@stanford.edu>
2026-01-27 13:33:52 -0800

    Add custom Zsh configuration files for Conda, NVM, SSH agent, and Zsh autosuggestions to enhance shell functionality and user experience.

---
+++ common/zsh/.oh-my-zsh/custom/ssh-agent.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+# Specify the paths to your private keys here to have them loaded on shell startup
+# For more information, see https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/ssh-agent
+zstyle :omz:plugins:ssh-agent lazy yes
+zstyle :omz:plugins:ssh-agent quiet yes
